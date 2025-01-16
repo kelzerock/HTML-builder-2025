890fd4b657ae0dcdefab0558db999a4dc4a35c32
Aleksei Zhuchkov <kelzzzer@gmail.com>
2025-01-16 23:27:08 +0300

    feat: complete task 05

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
+const pathToCSS = path.join(__dirname, 'styles');
+const pathToDist = path.join(__dirname, 'project-dist', 'bundle.css');
+
+const bundleCSS = (pathToCSS, pathToDist) => {
+  fs.readdir(pathToCSS, (err, files) => {
+    if (err) console.log(err);
+
+    const writeStream = fs.createWriteStream(pathToDist);
+    files.forEach((file) => {
+      const { ext } = path.parse(file);
+      if (ext === '.css') {
+        fs.readFile(path.join(pathToCSS, file), (err, info) => {
+          if (err) console.log(err);
+          writeStream.write(info.toString());
+        });
+      }
+    });
+  });
+};
+
+bundleCSS(pathToCSS, pathToDist);
+
+// code for testing test-files
+// just uncommit this
+
+// bundleCSS(
+//   path.join(__dirname, 'test-files', 'styles'),
+//   path.join(__dirname, 'test-files', 'bundle.css'),
+// );
